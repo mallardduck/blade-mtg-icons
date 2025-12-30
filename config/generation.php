33f56d20318edd3fbad21a380c6db25b7afe9581
Dan Pock <self@danpock.me>
2025-12-29 23:53:37 -0500

    Add git submodule and generation config

--- config/generation.php
+++ config/generation.php
@@ -1,8 +1,25 @@
 <?php
 
+$svgNormalization = static function (string $tempFilepath, array $iconSet) {
+    $doc = new DOMDocument();
+    $doc->load($tempFilepath);
+    $svgElement = $doc->getElementsByTagName('svg')[0];
+    $svgElement->removeAttribute('width');
+    $svgElement->removeAttribute('height');
+    $doc->save($tempFilepath);
+
+    $fileLines = file($tempFilepath);
+    array_shift($fileLines);
+
+    $lastKey = count($fileLines) - 1;
+    $fileLines[$lastKey] = trim($fileLines[$lastKey]);
+    file_put_contents($tempFilepath, $fileLines);
+};
+
 return [
     [
-        'source' => __DIR__.'/../node_modules/heroicons',
+        'source' => __DIR__.'/../Mana/svg',
         'destination' => __DIR__.'/../resources/svg',
+        'after' => $svgNormalization,
     ],
 ];
